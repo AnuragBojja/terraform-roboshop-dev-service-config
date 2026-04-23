17390b73e935aab9e472226f50deddcfe5613197
AnuragBojja <anuragbojja19@gmail.com>
2026-04-23 16:52:28 -0500

    Terraform-robosho-dev-service-config

--- variables.tf
+++ variables.tf
@@ -1,5 +1,4 @@
 variable "env" {
-  default = "dev"
 }
 variable "project_name" {
   default = "Roboshop"
@@ -8,7 +7,6 @@ variable "instance_type" {
   default = "t3.micro"
 }
 variable "domain_name" {
-  default = "anuragaws.shop"
 }
 variable "service_name" {
 }
